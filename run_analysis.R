f3fbddab5bc4e36ac1e0ac8ec871d8c30ac286c0
amachno <artur.machno@gmail.com>
2015-07-17 15:09:30 +0200

    data and code secon commit

--- run_analysis.R
+++ run_analysis.R
@@ -23,9 +23,24 @@ test.data<-cbind(test.object,test.activity,test.data)
 
 # bind test and train data
 data<-rbind(train.data, test.data)
-names(data[2])<- 'Activity'
+names(data)[2]<- 'Activity'
 
-# extract data of mean and std
-data<-data[, c(1,2,2+grep('mean\\(\\)|std\\(\\)', description))]
+# activities: class numbers and labels
+labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c ("Class", "Label") )
+data$Activity <- labels$Label[match(data$Activity,labels$Class)]
 
+# extract columns which contain mean and std values -- ignore letter case
+measures.mean.index <- grep ( "mean\\(\\)", colnames(data), ignore.case = TRUE )
+measures.std.index <- grep ( "std\\(\\)", colnames(data), ignore.case = TRUE)
+data.sub <- data[ c ( 1:2, measures.mean.index, measures.std.index ) ]
 
+# reshape2
+library (reshape2)
+
+# melt data set
+data.sub.melt <- melt(data.sub, id = c ("Subject", "Activity") , measure.vars = c(3:68), variable.name = "Features", value.name = "Signals" )
+
+# dcast by Subject & Activity, drop "Class", derive mean for Signals & adjust variable names appropriately
+data.tidy <- dcast (data.sub.melt, Subject + Activity ~ Features, mean, value.var = "Signals")
+#save the result
+write.table(data.tidy, 'tidy_data.txt', row.names=F)
